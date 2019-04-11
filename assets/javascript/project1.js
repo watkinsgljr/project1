071ebf2516278525dd0d60d33d0b0f874f1c4633
Stroupe79 <cstroupe79@gmail.com>
2019-04-11 09:19:36 -0500

    created the structure for ajax calls

---
+++ assets/javascript/project1.js
@@ -0,0 +1,44 @@
+
+
+$(document).ready(function () {
+
+    $(document).on("click", "#searchButton", function(){
+        var search = $("#searchField").val()
+        apiTM(search);
+    });
+
+
+
+
+
+
+
+
+
+
+
+
+function apiTM(eventSearch) {
+    console.log(eventSearch)
+    const apiKey = "apikey=rrFQUi7azSu6BIs8pNUwk9tDZHSTv8YY"
+    const apiTM = "https://app.ticketmaster.com/discovery/v2/"
+    let query = apiTM + "attractions.json?" + apiKey
+    console.log(query);
+    $.ajax({
+        url: query,
+        method: "GET"
+    }).then(function (response) {
+        for (i=10; i < 10; i++) {
+            console.log(response[i])
+            $("$results").text(response)
+        }
+    })
+};
+
+
+
+
+
+
+
+});
