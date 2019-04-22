9af054861e766c6ba5b8d57f44004d35f3412f17
Your NameGreg <you@example.comwatkinsgljr@gmail.com>
2019-04-22 11:24:31 -0500

    updated comment section logic..almost complete..ckecks database to see if there are comments posted upon event page load

--- assets/javascript/project1.js
+++ assets/javascript/project1.js
@@ -25,9 +25,13 @@ $(document).ready(function () {
     LOGGEDOFF: 1,
     LOGGEDIN: 2,
   };
+
+  let eventDetails = [];
+  let eventKey;
+
+
   //-------------------------------------------------DATA TABLE LOGIC------------------------------------------------
-  let database = firebase.database();
-  let queryObjectRef = database.ref("/queryObj")
+  const database = firebase.database();
   var data = [];
 
   table = $('#event-table').DataTable({
@@ -38,6 +42,8 @@ $(document).ready(function () {
     { title: "Location" },
     { title: "Date" }]
   });
+
+
 
   //-------------------------------------------------CREATE DATA TABLE BASED ON USER SEARCH-------------------------------
 
@@ -190,6 +196,74 @@ $(document).ready(function () {
     });
   };
 
+  //------------------------------------------------------------------------GOOGLE APIS--------------------------------------------------------------
+
+  function googleId(id) {
+    const googleKey = "key=AIzaSyAyJOOjrQqnT_rnAVL9Isx0SlP09SOvh5o";
+    const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+    const TARGET_URL = 'https://maps.googleapis.com/maps/api/place/findplacefromtext/json?&inputtype=textquery&input=' + id + "&" + googleKey + "&fields=place_id"
+    const URL = PROXY_URL + TARGET_URL
+    // cors solution = https://stackoverflow.com/questions/45185061/google-places-api-cors
+    $.ajax({
+      url: URL,
+      method: "GET",
+    }).then(function (answer) {
+      googlePlace(answer.candidates[0].place_id)
+    })
+  };
+
+  // function to gain detailed data using place_id
+
+  function googlePlace(venue) {
+    const googleKey = "key=AIzaSyAyJOOjrQqnT_rnAVL9Isx0SlP09SOvh5o";
+    const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+    // edit the fields of the below target URL to change the data we are getting back
+    const TARGET_URL = 'https://maps.googleapis.com/maps/api/place/details/json?&placeid=' + venue + "&" + googleKey + "&fields=url"
+    const URL = PROXY_URL + TARGET_URL
+    $.ajax({
+      url: URL,
+      method: "GET",
+    }).then(function (answer) {
+      $(".item-11").html("<iframe width='100%' height='250' frameborder='0' style='border:0' src='https://www.google.com/maps/embed/v1/place?" + googleKey + "&q=place_id:" + venue + "'></iframe>")
+
+    })
+  };
+
+  function googlePark(venue) {
+    const googleKey = "key=AIzaSyAyJOOjrQqnT_rnAVL9Isx0SlP09SOvh5o";
+    const PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
+    const TARGET_URL = 'https://www.google.com/maps/embed/v1/search?q=parking+near+'
+    const URL = PROXY_URL + TARGET_URL + venue + "&" + googleKey
+    // cors solution = https://stackoverflow.com/questions/45185061/google-places-api-cors
+    var map2;
+    $.ajax({
+      async: false,
+      url: URL,
+      method: "GET",
+      success: function () {
+        var map = "<iframe width='450' height='250' frameborder='0' style='border:0' src='" + TARGET_URL + venue + "&" + googleKey + "'></iframe>"
+        map2 = map;
+      }
+    })
+    mappingData(map2)
+
+
+
+    // .then (function (map){
+    //     // console.log(parking)
+    //     // console.log(TARGET_URL)
+    //     var map ="<iframe width='450' height='250' frameborder='0' style='border:0' src='" + TARGET_URL + venue + "&" + googleKey + "'></iframe>"
+    //     return map;
+    // })
+
+  }
+
+  function mappingData(mapData) {
+    parkingData = mapData
+  };
+
+
+
 
 
 
@@ -203,9 +277,11 @@ $(document).ready(function () {
   let eventTitle;
   let eventDate;
   let eventButton;
+  let gridLocation;
 
 
   function createEventCards(result, index) {
+    $(".grid-container").css("height", "110vh");
     if (index < 6) {
       $('#city-search').val("")
       $('#keyword-search').val("")
@@ -214,6 +290,7 @@ $(document).ready(function () {
 
       // ------ELEMENTS GENERATED AND ASSIGNED VARIABLE-----------
       eventCard = $("<div>");
+      eventCardBack = $("<div>");
       cardImage = $("<img>");
       cardBody = $("<div>");
       eventTitle = $("<a>");
@@ -222,7 +299,12 @@ $(document).ready(function () {
       eventButton = $("<a>");
       let eventURL;
       // EVENT CARD---------------------------------------------------
-      eventCard.addClass("card");
+      eventCard.addClass("card event-card front");
+      eventCardBack.addClass("card event-card back card-back");
+      eventCardBack.text("card event-card back");
+      eventCardBack.attr("autoSize", "false");
+      eventCardBack.attr("forceWidth", "true");
+      eventCardBack.attr("forceHeight", "true");
 
       //EVENT IMAGE---------------------------------------------------
       cardImage.addClass("card-img-top");
@@ -230,7 +312,7 @@ $(document).ready(function () {
       cardImage.attr("src", result.image);
       cardImage.prependTo(eventCard);
       //CARD BODY ELEMENTS--------------------------------------------
-      cardBody.addClass("card-body");
+      cardBody.addClass("card-body2");
       eventTitle.addClass("card-title");
       eventTitle.text(result.name)
       eventDate.addClass("card-text");
@@ -242,7 +324,9 @@ $(document).ready(function () {
       eventTitle.prependTo(cardBody);
       eventDate.appendTo(cardBody);
       eventButton.appendTo(cardBody);
-      eventCard.prependTo($(".item-" + gridLocation))
+      eventCard.appendTo($(".item-" + gridLocation));
+      eventCardBack.appendTo($(".item-" + gridLocation));
+      $(".item-" + gridLocation).flip();
     };
   }
 
@@ -261,12 +345,12 @@ $(document).ready(function () {
 
 
   $(document).on("click", ".event-card-btn", function () {
-    console.log($(this));   
+    console.log($(this));
     $("#event-page-main-container").show();
     $("#main-container").hide();
-    let buttonValue = $(this)[0].value
-    let eventDetails = $.grep(searchResults, function(events) {
-      return events.id == buttonValue;  
+    let buttonValue = $(this)[0].value;
+    eventDetails = $.grep(searchResults, function (events) {
+      return events.id == buttonValue;
     });
     console.log(eventDetails);
     console.log(eventButton.val());
@@ -274,16 +358,90 @@ $(document).ready(function () {
     $(".item-7").text(eventDetails[0].name);
     $(".event-page-date").text("Join us on " + eventDetails[0].date);
     $(".event-page-time").text("At " + eventDetails[0].time);
-
+    $("#comment-btn").val(eventButton.val());
+    eventKey = $("#comment-btn").val();
+    console.log($("#comment-btn").val());
+    $(".event-page-image").css({ "background-image": "url(" + eventDetails[0].image + ")", "background-size": "75% 75%", "margin": "auto", "background-position": "center" });
+    loadComments(eventKey);
+    // googleId(venueQuery);
   });
 
-  $(document).on("click", "#back-to-search-results-btn", function() {
+  $(document).on("click", "#back-to-search-results-btn", function () {
     event.preventDefault();
     $("#event-page-main-container").toggle();
     $("#main-container").toggle();
 
 
   })
+
+ //-------------------------------------------------CHECK FOR COMMENTS DATABASE---------------------------
+
+  function loadComments(eKey) {
+
+    database.ref("/commentsRef").once("value").then(function (snapshot) {
+      console.log(snapshot.val());
+      snapshotValue = snapshot.val();
+      snapshotValueArray = Object.values(snapshotValue);
+      console.log(snapshotValueArray);
+
+      let commentIdMatch = $.grep(snapshotValueArray, function (comment) {
+        return comment.eventId == eKey;
+      });
+      console.log(commentIdMatch);
+      if (commentIdMatch.length > 0) {
+        commentIdMatch.commentsRef.appendTo($("#comment-text-area"))
+      }
+    });
+  }
+
+  //-------------------------------------------------FLIP CARD CLICK EVENT------------------------------------
+
+  $(document).on("click", $(".item-" + gridLocation), function () {
+    $(this).flip();
+  })
+
+  //-------------------------------------------------COMMENTS SECTION LOGIC------------------------------------
+
+  $("#comment-btn").on("click", function (event) {
+    event.preventDefault();
+    $("#comment-text-area").text(" ");
+
+    let commentField = $('#comment-text-area').val();
+
+    database.ref("commentsRef").once("value").then(function (snapshot) {
+      console.log(snapshot);
+    })
+
+
+    database.ref("/eventsRef").push().set({
+      eventId: eventKey,
+      eventDetails: eventDetails[0]
+    });
+
+    database.ref("commentsRef").child.push().set({
+      eventId: eventKey,
+      name: "Greg",
+      message: commentField
+    });
+  });
+
+
+
+
+  //-------------CHAT EVENT LISTENER---------------
+
+
+  database.ref("/commentsRef").on('child_added', function (snapshot) {
+
+
+    var message = snapshot.val();
+    addComment(message.name, message.message);
+  });
+
+  function addComment(name, message) {
+    comment = name + ": " + message;
+    $("comment-div").prepend(comment);
+  }
 
 
 
